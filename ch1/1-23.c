a30729a42b3e23116b9ed8510e4e7501e91e5cf4
sdjdd <352207572@qq.com>
2018-08-29 10:02:16 +0800

    Add 1-23

---
+++ ch1/1-23.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define NORMAL     0
+#define SINGLELINE 1
+#define MULTILINE  2
+
+int main() {
+    int c, last, lastlast, mode, inStr = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '"')
+            inStr = !inStr;
+        if (!inStr) {
+            if (mode == SINGLELINE) {
+                if (c == '\n') {
+                    mode = NORMAL;
+                    putchar(c);
+                }
+            } else if (mode == MULTILINE) {
+                if (c == '/' && last == '*')
+                    mode = NORMAL;
+            } else if (last == '/') {
+                if (c == '/')
+                    mode = SINGLELINE;
+                else if (c == '*')
+                    mode = MULTILINE;
+                else if (lastlast != '*') {
+                    putchar(last);
+                    putchar(c);
+                }
+            } else if (c != '/')
+                putchar(c);
+        } else 
+            putchar(c);
+        lastlast = last;
+        last = c;
+    }
+
+    return 0;
+}
