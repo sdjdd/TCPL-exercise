60b946757109070366b6ce0bba607a05d31fcfcb
sdjdd <sdjddrt@gmail.com>
2018-09-05 09:00:12 +0800

    Update 2-6

--- ch2/2-6.c
+++ ch2/2-6.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int setbits(int x, int p, int n, int y); 
+unsigned setbits(unsigned x, int p, int n, unsigned y); 
 
 int main() {
     printf("%d", setbits(12, 3, 3, 3));
@@ -8,6 +8,6 @@ int main() {
     return 0;
 }
 
-int setbits(int x, int p, int n, int y) {
-    return x & (y << p+1-n) | (y << p+1-n);
+unsigned setbits(unsigned x, int p, int n, unsigned y) {
+    return x & ~(~(~0 << n) << (p+1-n)) | (y & ~(~0 << n)) << (p+1-n);
 }
